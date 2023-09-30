a8b8b90a01748435c72beb32c33cce33eace1789
Hithesh Jay <hithesh0215@gmail.com>
2023-09-30 23:44:03 +0530

    finally working!

--- FaceCNNModel.py
+++ FaceCNNModel.py
@@ -7,15 +7,15 @@ nb_classes = 7
 
 # Define the CNN for face detection
 class FaceCNN(nn.Module):
-    def __init__(self):
+    def __init__(self, nb_classes):
         super(FaceCNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 64,kernel_size=3, padding=1) # input channel=1 with 64 filters
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=3, padding=1)
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU()
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.pool = nn.MaxPool2d(kernel_size=2)
         self.dropout = nn.Dropout(0.25)
-        
-        self.conv2 = nn.Conv2d(64, 128,kernel_size=5, padding=2)
+
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=5, padding=2)
         self.bn2 = nn.BatchNorm2d(128)
 
         self.conv3 = nn.Conv2d(128, 512, kernel_size=3, padding=1)
@@ -24,50 +24,55 @@ class FaceCNN(nn.Module):
         self.conv4 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
         self.bn4 = nn.BatchNorm2d(512)
 
-        self.fc1 = nn.Linear(512,256)
+        self.fc1 = nn.Linear(512 * 3 * 3, 256)
+        self.bn_fc1 = nn.BatchNorm1d(256)
         self.fc2 = nn.Linear(256, 512)
-        self.fc3= nn.Linear(512, nb_classes)
-        
+        self.bn_fc2 = nn.BatchNorm1d(512)
+        self.fc3 = nn.Linear(512, nb_classes)
+
     def forward(self, x):
+       #first layer
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
         x = self.pool(x)
         x = self.dropout(x)
-
+        #second layer
         x = self.conv2(x)
         x = self.bn2(x)
         x = self.relu(x)
         x = self.pool(x)
         x = self.dropout(x)
-
+        #third layer
         x = self.conv3(x)
         x = self.bn3(x)
         x = self.relu(x)
         x = self.pool(x)
         x = self.dropout(x)
-
+        #4th layer
         x = self.conv4(x)
         x = self.bn4(x)
         x = self.relu(x)
         x = self.pool(x)
         x = self.dropout(x)
 
-        x = x.view(x.size(0), -1)  # Flattening 
+        x = x.view(x.size(0), -1)  #flattening
 
+        # fully connected layer 1
         x = self.fc1(x)
-        x = self.bn1(x)
+        x = self.bn_fc1(x)
         x = self.relu(x)
         x = self.dropout(x)
-        
+
+        # fully connected layer 2
         x = self.fc2(x)
-        x = self.bn2(x)
+        x = self.bn_fc2(x)
         x = self.relu(x)
         x = self.dropout(x)
-        
+
+        # fully connected layer 3
         x = self.fc3(x)
-        x = torch.softmax(x, dim=1)
 
         return x
 
-model = FaceCNN()
+model = FaceCNN(nb_classes)
--- predict.py
+++ predict.py
@@ -2,10 +2,11 @@ import cv2
 import torch
 from FaceCNNModel import FaceCNN
 import torchvision.transforms as transforms
-import numpy as np
+import torch.nn.functional as F
+nb_classes = 7
+model = FaceCNN(nb_classes)
 
-model = FaceCNN()
-state_dict  = torch.load('')
+state_dict  = torch.load('pytorch_weights/best weights.pth',map_location=torch.device('cpu'))
 model.load_state_dict(state_dict)
 
 haar_file=cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
@@ -16,21 +17,28 @@ def detect_emotions(frame):
     faces=face_cascade.detectMultiScale(frame,1.3,5)
 
     for (x,y,w,h) in faces:
-        #obtaining the face coordinates
 
+        #obtaining the face coordinates
         image = gray[y:y+h,x:x+w]
         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
         
         #resizing the face image
         transform = transforms.Compose([
-            transforms.Resize((48,48)),
-            transforms.ToTensor(),
-            transforms.Normalize((0.5,),(0.5,))
+        transforms.ToPILImage(),
+        transforms.Resize((48, 48)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))
         ])
 
-        #unsqueeze to add a new dimention at position 0
-        #(1,48,48) -> (1,1,48,48)
-        image = transform(image).unsqueeze(0) 
+        #unsqueeze to add a new dimention at position 0 |(1,48,48) -> (1,1,48,48)
+        #convert numpy array to torch tensor
+        
+        #print(torch.from_numpy(image)).unsqueeze(0)
+
+        #img_tensor = torch.from_numpy(image)
+
+        image = transform(image).unsqueeze(0)
+
         labels = {0 : 'angry', 1 : 'disgust', 2 : 'fear', 3 : 'happy', 4 : 'neutral', 5 : 'sad', 6 : 'surprise'}
 
         #setting to evaluation mode
@@ -39,8 +47,10 @@ def detect_emotions(frame):
         #disabling gradient calculation
         with torch.no_grad(): 
             feature = image
-            pred = model(feature)
-            prediction_label = labels[pred.argmax()]
+            logits = model(feature)
+            probs = F.softmax(logits, dim=1)
+            # argmax returns a tensor we convert it to a integer
+            prediction_label = labels[probs.argmax().item()]
 
         print("Predicted Output:", prediction_label)
 
